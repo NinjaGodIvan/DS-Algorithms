c8f9daffc10dfe549eb05fc8c2d31c3eabf469db
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-09-25 02:41:31 -0700

    Update BinaryTree.cpp

--- C++/BinaryTree.cpp
+++ C++/BinaryTree.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-using nsamespace std;
+using namespace std;
 
 struct Node{
   int data;
-  Node *left = nullptr, *right = *nullptr;
+  Node *left = nullptr, *right = nullptr;
   Node(int data){
     this->data = data;
   }
@@ -11,32 +11,49 @@ struct Node{
 
 class BinaryTree{
   public:
-    void insertNode(int);
-    void preOrder();
-    void inOrder();
-    void postOrder();
+    void insertNode(Node*, int);
+    void preOrder(Node*);
+    void inOrder(Node*);
+    void postOrder(Node*);
     Node *getRoot()
-    {return this.root};
+    {return this->root;}
   private:
     Node *root = nullptr;
-}
+};
 
 int main(){
   
- BinaryTree tree = new BinaryTree();
- tree->insertNode(tree->getRoot(), 10);
+    BinaryTree *tree = new BinaryTree();
+    tree->insertNode(tree->getRoot(), 10);
+    tree->insertNode(tree->getRoot(), 20);
+    tree->insertNode(tree->getRoot(), 30);
+    
+    cout <<"List of nodes using In Order method:\n";
+    tree->inOrder(tree->getRoot());
 
-  return 0;
+    return 0;
 }
 
-void BinaryTree::insertNode(Node *root, int data){
-  if(!root){
+/* Inserts node into the Binary Tree */
+void BinaryTree::insertNode(Node *node, int data){
+  if(!root){ //If the tree is empty, add a root
     root = new Node(data);
-  } else {
-    if(data > root->data){
-      insertNode(root->right)
+  } else if(node){ //If the tree contains a root, then traverse
+    if(data > node->data){
+      insertNode(node->right, data);
     } else {
-      insertNode(root->left)
+      insertNode(node->left, data);
     }
+  } else { //Adds a node
+      node = new Node(data);
   }
+}
+
+/* Prints nodes using InOrder method */
+void BinaryTree::inOrder(Node *root){
+    if(root){
+        inOrder(root->left);
+        cout << root->data <<" ";
+        inOrder(root->right);
+    }
 }
